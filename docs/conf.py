c3bf44b7805a42a163b39713bfcccff826b8f91e
Shashank Shekhar <contactshashankshekhar@gmail.com>
2025-06-07 23:02:33 -0400

    granted root permission to readthedocs, updated author name

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 project = 'MinText'
 copyright = '2025'
-author = 'MinText Team'
+author = 'Shashank Shekhar'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
