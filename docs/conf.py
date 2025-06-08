8eed022ca8f6ff77e8fb08398f4393dd4722ac05
Shashank Shekhar <contactshashankshekhar@gmail.com>
2025-06-07 22:41:24 -0400

    init: initialized readthedocs and added first tutorial notebook

---
+++ docs/conf.py
@@ -0,0 +1,63 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'MinText'
+copyright = '2025'
+author = 'MinText Team'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.mathjax',
+    'nbsphinx',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# -- nbsphinx configuration --------------------------------------------------
+# Execute notebooks before conversion: 'always', 'never', 'auto' (default)
+nbsphinx_execute = 'never'
+
+# If True, the build process is continued even if an exception occurs
+nbsphinx_allow_errors = True
+
+# This is processed by Jinja2 and inserted before each notebook
+nbsphinx_prolog = r"""
+{% set docname = 'docs/' + env.doc2path(env.docname, base=None) %}
+
+.. note::
+
+   This page was generated from `{{ docname }}`__.
+   
+   __ https://github.com/username/MinText/blob/main/{{ docname }}
+"""
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# Create necessary folders for Sphinx
+html_static_path = ['_static']
+os.makedirs(os.path.join(os.path.dirname(__file__), '_static'), exist_ok=True)
+os.makedirs(os.path.join(os.path.dirname(__file__), '_templates'), exist_ok=True)
---
+++ docs/tutorials.rst
@@ -0,0 +1,9 @@
+Tutorials
+=========
+
+This section contains tutorials for using MinText.
+
+.. toctree::
+   :maxdepth: 2
+   
+   tutorials/1_JAX_Parallelization
